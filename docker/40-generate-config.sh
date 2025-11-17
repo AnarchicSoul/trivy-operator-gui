dd165dbee782a30fa2820044a6c4fe9a2845bab8
Johan Duval <JD52130524@ACS-INC.FR>
2025-11-17 19:19:19 +0100

    Fix entrypoint script - use sed instead of envsubst to preserve nginx variables

--- docker/40-generate-config.sh
+++ docker/40-generate-config.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
-# Generate nginx config from template with environment variable substitution
+# Generate nginx config from template with BACKEND_SERVICE_NAME substitution
 echo "Generating nginx configuration with BACKEND_SERVICE_NAME="
-envsubst '' < /tmp/nginx-template.conf > /etc/nginx/conf.d/default.conf
+
+# Use sed to replace only , leaving nginx $ variables untouched
+sed "s/${BACKEND_SERVICE_NAME}//g" /tmp/nginx-template.conf > /etc/nginx/conf.d/default.conf
 
 echo "Nginx configuration generated successfully"
 cat /etc/nginx/conf.d/default.conf
