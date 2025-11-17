21b1557c02f0295c2a29294d0b84f3ae1229f2ca
Johan Duval <JD52130524@ACS-INC.FR>
2025-11-17 19:24:46 +0100

    Fix nginx template - restore all nginx variables that were corrupted

--- docker/40-generate-config.sh
+++ docker/40-generate-config.sh
@@ -4,8 +4,9 @@ set -e
 # Generate nginx config from template with BACKEND_SERVICE_NAME substitution
 echo "Generating nginx configuration with BACKEND_SERVICE_NAME=${BACKEND_SERVICE_NAME}"
 
-# Use sed to replace only ${BACKEND_SERVICE_NAME}, leaving nginx $ variables untouched
-sed "s/\${BACKEND_SERVICE_NAME}/${BACKEND_SERVICE_NAME}/g" /tmp/nginx-template.conf > /etc/nginx/conf.d/default.conf
+# Use sed with explicit variable to replace ${BACKEND_SERVICE_NAME}
+BACKEND_NAME="${BACKEND_SERVICE_NAME}"
+sed "s/\${BACKEND_SERVICE_NAME}/${BACKEND_NAME}/g" /tmp/nginx-template.conf > /etc/nginx/conf.d/default.conf
 
 echo "Nginx configuration generated successfully"
 cat /etc/nginx/conf.d/default.conf
