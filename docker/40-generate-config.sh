eccf243cd93797a3aea0d190cd1336bd0c6fb7bf
Johan Duval <JD52130524@ACS-INC.FR>
2025-11-17 19:16:50 +0100

    Fix line endings - convert CRLF to LF for shell script and nginx template

--- docker/40-generate-config.sh
+++ docker/40-generate-config.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Generate nginx config from template with environment variable substitution
-echo "Generating nginx configuration with BACKEND_SERVICE_NAME=${BACKEND_SERVICE_NAME}"
-envsubst '${BACKEND_SERVICE_NAME}' < /tmp/nginx-template.conf > /etc/nginx/conf.d/default.conf
+echo "Generating nginx configuration with BACKEND_SERVICE_NAME="
+envsubst '' < /tmp/nginx-template.conf > /etc/nginx/conf.d/default.conf
 
 echo "Nginx configuration generated successfully"
 cat /etc/nginx/conf.d/default.conf
